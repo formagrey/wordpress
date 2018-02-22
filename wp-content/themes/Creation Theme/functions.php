3a1efb4392ad1fdaf5748469f1bb1f50fa4bec4b
MikeMoleka <mike.moleka@gmail.com>
2018-02-22 09:19:13 +0100

    sidebar

--- wp-content/themes/Creation Theme/functions.php
+++ wp-content/themes/Creation Theme/functions.php
@@ -11,6 +11,7 @@
     add_action( 'wp_enqueue_scripts', 'enqueue_bootstrap' );*/
     // This function enqueues the Normalize.css for use. The first parameter is a name for the stylesheet, the second is the URL. Here we
 // use an online version of the css file.
+    if ( function_exists('register_sidebar') ) register_sidebar(2);
     function add_normalize_CSS() {
         wp_enqueue_style( 'normalize-styles', "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css");
     }
